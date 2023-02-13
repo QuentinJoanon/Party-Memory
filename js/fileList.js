857fe8609acfa6596425538c6a05aae2da370fb6
QuentinJoanon <quentin.joanon@gmail.com>
2023-02-13 23:01:43 +0100

    Restructuration et début de création de la gallery

---
+++ js/fileList.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
+let fileList = [];
+
+fs.readdir('./uploads', (err, files) => {
+  if (err) {
+    console.error(err);
+  } else {
+    for (const file of files) {
+      if (path.extname(file) === '.jpg') {
+        fileList.push(file);
+      }
+    }
+    console.log(fileList);
+  }
+});
+
+
+
+module.exports = fileList ;
--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,23 @@
+const PORT = process.env.PORT || 3000;
 const express = require('express');
 const multer = require('multer');
 const app = express();
 const path = require('path');
+const fileList = require('../../js/fileList');
 
+
+
+app.set("views","./views");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res)=>{
+  res.render("index");
+});
+
+app.get("/gallery", (req, res)=>{
+  res.render("gallery");
+});
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -19,7 +34,9 @@ app.post('/upload', upload.single('photo'), (req, res) => {
   if (!req.file) {
     return res.sendStatus(400);
   }
-  res.send("Fichier uploadé avec succès");
+  res.render("upload");
 });
 
-app.listen(3000, () => console.log('Server started on port 3000'));
+app.listen(PORT, ()=>{
+  console.log(`Serveur lancé sur http://localhost:${PORT}`);
+});
