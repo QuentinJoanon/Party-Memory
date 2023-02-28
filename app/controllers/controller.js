13286457c4d4c1e01a44f6c5b4a610000c113e50
Qwikle <qwikle.ziko@gmail.com>
2023-02-28 11:42:17 +0100

    make read file as promise

--- app/controllers/controller.js
+++ app/controllers/controller.js
@@ -1,25 +1,26 @@
-const fileList = require('../js/fileList.js');
+const fileList = require("../js/fileList.js");
 
 const controller = {
   /**
    * Affichage de la page principale
    * Parametre "articles"
    */
-  homePage(req, res){
+  homePage(req, res) {
     res.render("index");
   },
   /**
    * Affichage de la galerie
    * Parametre "fileList"
    */
-  galleryPage(req, res){
-    res.render("gallery", {fileList});
+  async galleryPage(req, res) {
+    const images = await fileList();
+    res.render("gallery", { fileList: images });
   },
   /**
    * Affichage de la page upload
    * Pas de paramètres
    */
-  uploadPage(req, res){
+  uploadPage(req, res) {
     res.render("upload");
   },
 };
--- app/js/fileList.js
+++ app/js/fileList.js
@@ -1,21 +1,22 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-let fileList = [];
-
-fs.readdir('public/uploads', (err, files) => {
-  if (err) {
-    console.error(err);
-  } else {
-    for (const file of files) {
-      if (path.extname(file) === '.jpg') {
-        fileList.push(file);
+const regex = new RegExp(/(jpg|jpeg|png|gif|JPG|JPEG|PNG|GIF)$/);
+function getAllImages() {
+  return new Promise((resove, reject) => {
+    fs.readdir(path.join(__dirname, "../../public/uploads"), (err, files) => {
+      if (err) {
+        reject(err);
+      } else {
+        for (const file of files) {
+          if (!regex.test(path.extname(file))) {
+            files.splice(file, 1);
+          }
+        }
+        resove(files);
       }
-    }
-    console.log(fileList);
-  }
-});
+    });
+  });
+}
 
-
-
-module.exports = fileList ;
+module.exports = getAllImages;
